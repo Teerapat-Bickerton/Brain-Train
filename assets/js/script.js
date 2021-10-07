a139c519656101fa7e04cf3e377283f2302d6705
Teerapat Bickerton <teebickerton@gmail.com>
2021-10-07 14:32:39 +0000

    Add functions to JavaScript and add code to runGame fucntion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
+const shuffledQuestions, currentQuestionIndex;
+
+// Wait for the DOM to finish loading before running game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    runGame();
+
+})
+
+
+// Copied code from https://www.youtube.com/watch?v=riDzcEQbX6k&t=813s&ab_channel=WebDevSimplified
 function runGame() {
+
+    console.log('started');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    displayNextQuestion();
 
 }
 
-function displayQuestion() {
+function displayNextQuestion() {
 
 }
 
@@ -15,5 +33,15 @@ function incrementScore() {
 }
 
 function incrementWrong() {
-    
+
 }
+
+const questions = [
+    {
+        question: 'What is the biggest animal in the world?',
+        answers: [
+            { text: 'Blue Whale', correct: true},
+            { text: 'Finn Whale', correct: false},
+        ]
+    }
+]
