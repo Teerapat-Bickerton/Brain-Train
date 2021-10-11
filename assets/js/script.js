73fbcaca5935deb064aac549330490d43c217ab2
Teerapat Bickerton <teebickerton@gmail.com>
2021-10-11 18:14:59 +0000

    Add code to reset state function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Used code from https://www.youtube.com/watch?v=riDzcEQbX6k&t=813s&ab_channel=WebDevSimplified
-const shuffledQuestions, currentQuestionIndex;
+const nextButton = document.getElementById('next-btn')
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
 
 // Wait for the DOM to finish loading before running game
 // Get the button elements and add event listeners to them
@@ -25,15 +27,37 @@ function runGame() {
 
 function displayNextQuestion() {
 
+    resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 
 }
 
 function showQuestion(question) {
 
-} 
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button)
+    })
 
-function checkAnswer() {
+}
+
+function resetState() {
+
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+
+  }
+
+function selectAnswer() {
 
 }
 
